22ab21c12bddddd7fb720ea9625ae0126aeba82c
Vincent Bernat <bernat@luffy.cx>
2012-06-10 01:14:22 +0200

    Remove non-async hack: it does not work.

--- gnus-servers.el
+++ gnus-servers.el
@@ -15,15 +15,6 @@
 				     "Sent"))
       gnus-message-archive-method '(nnimap ""))
 
-;; Hack to circumvent some problem with new async code in Gnus
-;; See: http://git.gnus.org/cgit/gnus.git/commit/?id=8eb5f1cde674860ba1a05c8754e45dbadc137ca5
-(defun vbe/gnus-hack-startup ()
-  (let ((funcs (nthcdr 3 (assoc 'nntp
-				nnoo-definition-alist))))
-    (setcar funcs (delq 'nntp-finish-retrieve-group-infos
-			(delq 'nntp-retrieve-group-data-early (car funcs))))))
-(add-hook 'gnus-started-hook 'vbe/gnus-hack-startup)
-
 ;; Scan news every 5 minutes if idle for more than 30 seconds
 (gnus-demon-add-handler 'gnus-demon-scan-news 5 30)
 
