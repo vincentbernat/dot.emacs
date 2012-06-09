fd0331ff4f6b9c71011c30f716b5c623a0d24610
Vincent Bernat <bernat@luffy.cx>
2012-06-10 00:38:26 +0200

    coffee: default tab width to 2

--- programming.el
+++ programming.el
@@ -6,7 +6,8 @@
 
 ;; Various packages
 (vbe/add-package '(:name "dtrt-indent")) ; autodetect indentation
-(vbe/add-package '(:name "coffee-mode")) ; coffee
+(vbe/add-package (list :name "coffee-mode"
+		       :init '(setq coffee-tab-width 2))) ; coffee
 (vbe/add-package '(:name "auctex"))	 ; latex
 (vbe/add-package '(:name
 		   "markdown-mode"))	 ; markdown
