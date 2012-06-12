4c08fec1d9127808d0bcbb74d111d7bf31c9a9fa
Vincent Bernat <bernat@luffy.cx>
2012-06-13 00:44:17 +0200

    programming: add a C style "openbsd"
    Should be compatible with FreeBSD:
     http://svnweb.freebsd.org/base/stable/9/tools/tools/editing/freebsd.el?view=markup

--- programming.el
+++ programming.el
@@ -20,4 +20,24 @@
       c-basic-offset 4                  ; with a 4 spaces indent
       indent-tabs-mode nil)             ; don't use tabs
 
+;; More styles. To debug, use C-c C-s.
+;;  - `+` means `c-basic-offset` times 1.
+;;  - `*` means `c-basic-offset` times 0.5.
+(c-add-style
+ "openbsd"                     ; Should be compatible with FreeBSD too
+ '("bsd"
+   (c-basic-offset . 8)
+   (c-tab-width . 8)
+   (fill-column . 80)
+   (indent-tabs-mode . t)
+   (c-offsets-alist . ((defun-block-intro     . +)
+                       (statement-block-intro . +)
+                       (statement-case-intro  . +)
+                       (statement-cont        . *)
+                       (substatement-open     . *)
+                       (substatement          . +)
+                       (arglist-cont-nonempty . *)
+                       (inclass               . +)
+                       (knr-argdecl-intro     . +)))))
+
 (provide 'vbe/programming)
