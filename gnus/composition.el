3487d7770d674760e7d09dd8732eec717859bdf3
Vincent Bernat <bernat@luffy.cx>
2012-06-14 23:19:16 +0200

    gnus: use an alternate citation line

--- gnus/composition.el
+++ gnus/composition.el
@@ -1,3 +1,5 @@
-;; Nothing, yet!
+;; Citation format
+(setq message-citation-line-function 'message-insert-formatted-citation-line
+      message-citation-line-format " ❦ %e %B %Y %R %Z, %f :\n")
 
 (provide 'vbe/gnus/composition)
