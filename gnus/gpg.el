9d7281ab218f01ce30a27c23cbc99b3a2105c1ca
Vincent Bernat <bernat@luffy.cx>
2012-07-08 22:11:29 +0200

    gpg: switch to authinfo.gpg
    Therefore, we can ask to save passwords to authinfo again. And also,
    we use public-key encryption instead of symmetric encryption.

--- gnus/gpg.el
+++ gnus/gpg.el
@@ -1,6 +1,7 @@
 (require 'epg)
 (setq mml2015-use 'epg		 ; use epg
       mm-verify-option 'always	 ; always check for sigs
-      mm-decrypt-option 'always) ; always decrypt
+      mm-decrypt-option 'always  ; always decrypt
+      auth-source-gpg-encrypt-to user-mail-address)
 
 (provide 'vbe/gnus/gpg)
