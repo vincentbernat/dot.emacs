509cfbba96074cb9eca98b04de87328518a9d0a4
Vincent Bernat <bernat@luffy.cx>
2012-07-24 22:21:09 +0200

    appearance: set XFT fonts only when using X11

--- appearance.el
+++ appearance.el
@@ -31,8 +31,9 @@
   (load-theme 'naquadah t)
   ; We could use `custom-theme-set-faces' but we prefer to use
   ; `set-face-attribute' since we can redefine only part of a face.
-  (dolist (face `(mode-line minibuffer-prompt))
-    (set-face-attribute face nil :font "DejaVu Sans-10"))
+  (when (eq 'x (window-system))
+    (dolist (face `(mode-line minibuffer-prompt))
+      (set-face-attribute face nil :font "DejaVu Sans-10")))
   (when (fboundp 'gnus-message) (vbe/gnus/custom-theme))
   (eval-after-load "gnus"
     '(add-hook 'gnus-started-hook 'vbe/gnus/custom-theme)))
@@ -40,8 +41,9 @@
 (defun vbe/gnus/custom-theme ()
   "Custom theme, for Gnus"
   (set-face-attribute 'gnus-summary-selected-face nil :underline nil)
-  (dolist (face `(content from subject name))
-    (set-face-attribute (intern (format "gnus-header-%s-face" face))
-			nil :font "DejaVu Sans-10")))
+  (when (eq 'x (window-system))
+    (dolist (face `(content from subject name))
+      (set-face-attribute (intern (format "gnus-header-%s-face" face))
+			  nil :font "DejaVu Sans-10"))))
 
 (provide 'vbe/appearance)
