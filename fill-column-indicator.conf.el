eeac0b8262024b5773c5c43ca107f14f7fd3634b
Vincent Bernat <bernat@luffy.cx>
2013-02-02 16:19:30 +0100

    fci: when loading for the first time, fill-column is nil
    Use default-fill-column instead.

--- fill-column-indicator.conf.el
+++ fill-column-indicator.conf.el
@@ -1,5 +1,5 @@
 ;; fill column indicator configuration
 (setq fci-rule-width 1
-      fci-rule-column (+ 5 fill-column)
+      fci-rule-column (+ 5 default-fill-column)
       fci-rule-character ?│
       fci-rule-color "DarkOrange1")
