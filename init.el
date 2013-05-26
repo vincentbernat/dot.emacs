814e061dab0928c7ac0ac17bc0cd5b595aa936b2
Vincent Bernat <bernat@luffy.cx>
2013-05-26 18:57:11 +0200

    bindings: move bindings to a dedicated file

--- init.el
+++ init.el
@@ -60,16 +60,7 @@ be searched in \"el-get.el\" in the user Emacs directory."
       x-stretch-cursor t)    ; stretch cursor to the width of the char
 
 ;; Bindings
-(global-set-key (kbd "M-g") 'goto-line)
-(global-set-key (kbd "<delete>") 'delete-char)
-(global-set-key (kbd "M-/") 'hippie-expand)
-(global-set-key (kbd "C-x g") 'magit-status)
-(global-set-key (kbd "C-c C-SPC") 'ace-jump-mode)
-(global-unset-key "\C-x\C-c")           ; Too easy to hit by
-                                        ; accident. Never used. Use
-                                        ; kill-emacs directly
-(when window-system
-  (global-unset-key "\C-z"))
+(vbe/require 'bindings)
 
 ;; Automode
 (add-to-list 'auto-mode-alist '("-MIB$" . snmpv2-mode))
