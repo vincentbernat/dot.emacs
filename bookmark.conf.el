1e9f294fe0a84f546f42853d4e4d8a22cf8aef6b
Vincent Bernat <vincent.bernat@dailymotion.com>
2013-09-09 11:15:40 +0200

    bookmark: add configuration file for bookmarks

---
+++ bookmark.conf.el
@@ -0,0 +1,5 @@
+;; Bookmark configuration
+(setq
+ bookmark-save-flag 1             ; Save bookmarks at each modification
+ bookmark-default-file (expand-file-name "run/bookmarks"
+                                         user-emacs-directory))
