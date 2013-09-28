1ad203071e971ac024e26ec5ce886df1b5ba72da
Vincent Bernat <bernat@luffy.cx>
2013-09-28 14:30:47 +0200

    auto-complete: don't use up/down array to navigate completions
    It is too frequent to being grabbed into the completion system while
    we only want to navigate in the buffer. alt-p, alt-n are still
    available for completion.

--- auto-complete.conf.el
+++ auto-complete.conf.el
@@ -1,3 +1,7 @@
 (add-to-list 'ac-dictionary-directories (vbe:run-directory "ac-dict"))
 (require 'auto-complete-config)
 (ac-config-default)
+
+;; Don't use up/down arrow (use M-n, M-p only) to browse list
+(define-key ac-completing-map [down] nil)
+(define-key ac-completing-map [up] nil)
