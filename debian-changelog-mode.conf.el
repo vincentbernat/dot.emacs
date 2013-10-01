8215974b453fdd86f13681423d0d60522cb1d4de
Vincent Bernat <bernat@luffy.cx>
2013-10-01 16:15:00 +0200

    debian: use UNRELEASED as the default distribution

--- debian-changelog-mode.conf.el
+++ debian-changelog-mode.conf.el
@@ -1,2 +1,6 @@
 ;; My Debian address
 (setq debian-changelog-mailing-address "bernat@debian.org")
+
+;; Add UNRELEASE at the front place
+(add-to-list 'debian-changelog-allowed-distributions "UNRELEASED" nil
+             (lambda (a b) nil))
