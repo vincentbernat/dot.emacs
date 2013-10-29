a3bedc581cd6c86be3fe4e76941f09aa389d12c2
Vincent Bernat <bernat@luffy.cx>
2013-10-29 22:47:54 +0100

    init: another try at getting a speedier font-lock-mode

--- init.el
+++ init.el
@@ -46,6 +46,7 @@ be searched in \"el-get.el\" in the user Emacs directory."
 (column-number-mode 1)			; show column number
 (global-hl-line-mode 1)			; highlight current line
 (mouse-avoidance-mode 'jump)		; move the mouse away
+(setq font-lock-maximum-decoration 2)   ; faster font-lock-mode
 (set-default 'indicate-buffer-boundaries '((up . nil) (down . nil) (t . left)))
 (require 'naquadah-theme)
 
