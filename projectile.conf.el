17b44265b059e4828345966cd39da2736ace12d9
Vincent Bernat <bernat@luffy.cx>
2013-11-02 11:06:31 +0100

    projectile: detect SVN projects

--- projectile.conf.el
+++ projectile.conf.el
@@ -1,3 +1,7 @@
 (let ((dir (vbe:run-directory "projectile")))
   (setq projectile-known-projects-file (expand-file-name "bookmarks.eld" dir)
         projectile-cache-file (expand-file-name "projectile.cache" dir)))
+
+;; Detect SVN projects
+(add-to-list 'projectile-project-root-files ".svn")
+(add-to-list 'projectile-globally-ignored-directories ".svn")
