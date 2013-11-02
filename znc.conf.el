837c895bce4d5fe7a8c6101572187014d529adda
Vincent Bernat <bernat@luffy.cx>
2013-11-02 23:29:50 +0100

    znc: provide network name in password
    We use the same ZNC server for many networks. In this case, the
    password should be `login/network:password`. This is not something
    that ZNC.el is handling. Therefore, we put the network name into the
    login and we redefine `znc-erc-ssl-connector` to retrieve this network
    name and build the correct password.
    
    A cool side-effect is that the password is no longer displayed in the
    messages.

--- znc.conf.el
+++ znc.conf.el
@@ -1,23 +1,39 @@
-(defun vbe/znc-add-server (server port ssl user networks)
+(defun vbe/znc-add-server (server port user networks)
   "Add a server to the list of ZNC servers.
 
-The password will be retrieved with `auth-source-search'."
-  (let ((found (nth 0 (auth-source-search :host server
-                                          :user user
-                                          :require '(:user :secret)
-                                          :max 1))))
+We use SSL inconditionaly. Moreover, we don't store the password
+but put nil instead. At least, we tweak the username to contain
+the network name later, this will be separated again."
+  (add-to-list 'znc-servers
+               (list server
+                     port
+                     t                  ; SSL enabled
+                     (mapcar (function (lambda (slug) (list slug
+                                                            (format "%s/%s" user slug)
+                                                            nil)))
+                                     networks))))
+
+(defun vbe/znc-erc-ssl-connector (&rest R)
+  "Connect to ERC using SSL and retrieve password with `auth-source-search'.
+
+Moreover, handle multiple networks by sending the password with
+the appropriate network slug that we extract from the nick."
+  (let* ((user (nth 0 (split-string (plist-get R :nick) "/")))
+         (slug (nth 1 (split-string (plist-get R :nick) "/")))
+         (found (nth 0 (auth-source-search :host (plist-get R :server)
+                                           :user user
+                                           :require '(:user :secret)
+                                           :max 1))))
     (if found
-        (let ((user (plist-get found :user))
-              (password (let ((secret (plist-get found :secret)))
+        (let ((password (let ((secret (plist-get found :secret)))
                           (if (functionp secret)
                               (funcall secret)
                             secret))))
-          (add-to-list 'znc-servers
-                       (list server
-                             port
-                             ssl
-                             (mapcar (function (lambda (x) (list x user password)))
-                                     networks)))))))
+          (plist-put R :password (format "%s/%s:%s" user slug password))
+          (plist-put R :nick user)
+          (apply 'erc-tls R)))))
+(setq znc-erc-ssl-connector 'vbe/znc-erc-ssl-connector)
 
-(vbe/znc-add-server "znc.luffy.cx" 7667 t "bernat"
+;; Define networks
+(vbe/znc-add-server "znc.luffy.cx" 7667 "bernat"
                     '(oftc freenode))
