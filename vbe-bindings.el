e812dbc59284e357a5a395fed620bbcc3bb7b27e
Vincent Bernat <vincent.bernat@dailymotion.com>
2013-11-04 12:35:04 +0100

    bindings: bind ace-jump to C-c j

--- vbe-bindings.el
+++ vbe-bindings.el
@@ -2,7 +2,7 @@
 (global-set-key (kbd "<delete>") 'delete-char)
 (global-set-key (kbd "M-/") 'hippie-expand)
 (global-set-key (kbd "C-x g") 'magit-status)
-(global-set-key (kbd "C-c C-j") 'ace-jump-mode)
+(global-set-key (kbd "C-c j") 'ace-jump-mode)
 (global-unset-key "\C-x\C-c")           ; Too easy to hit by
                                         ; accident. Never used. Use
                                         ; kill-emacs directly
