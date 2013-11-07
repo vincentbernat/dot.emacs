85e6616be64d0408b99e032d935f04e089d82171
Vincent Bernat <bernat@luffy.cx>
2013-11-07 09:41:05 +0100

    ido: use virtual buffers

--- ido.conf.el
+++ ido.conf.el
@@ -1,9 +1,10 @@
 ;; IDO mode everywhere
 (require 'flx-ido)
 (require 'ido-vertical-mode)
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
+(setq ido-enable-flex-matching t        ; IDO flex matching
+      ido-everywhere t                  ; use IDO whenever possible
+      ido-use-virtual-buffers t         ; use closed buffers as well
+      ido-use-faces nil)                ; let flx-ido do this job
 (ido-mode 1)
 (ido-vertical-mode 1)
 (flx-ido-mode 1)
-(setq ido-use-faces nil)
