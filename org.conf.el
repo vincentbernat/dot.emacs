6d98cd15995e78c18af0485f2e15df9ca1a1c894
Vincent Bernat <bernat@luffy.cx>
2013-12-05 08:37:56 +0100

    org: move org-protocol configuration to a dedicated file

--- org.conf.el
+++ org.conf.el
@@ -72,21 +72,4 @@
          :foreground "white"
          :weight bold)))
 
-;; Setup org-protocol with XDG
 (require 'org-protocol)
-(let ((desktop-file (expand-file-name "~/.local/share/applications/emacsclient.desktop")))
-  (unless (file-exists-p desktop-file)
-    (with-temp-file desktop-file
-      (insert "[Desktop Entry]
-Name=Emacs Client
-Exec=emacsclient %u
-Icon=emacs-icon
-Type=Application
-Terminal=false
-MimeType=x-scheme-handler/org-protocol;
-"))
-    (call-process "update-desktop-database" nil nil nil (file-name-directory desktop-file))
-    (call-process "xdg-mime" nil nil nil "default" "emacsclient.desktop" "x-scheme-handler/org-protocol")))
-
-;; Here is the associated bookmarklet:
-;; javascript:location.href='org-protocol://store-link://'+encodeURIComponent(location.href)+'/'+encodeURIComponent(document.title)+'/'+encodeURIComponent(window.getSelection())
