e91351723e453d792ce5958732a088311587ac3c
Vincent Bernat <bernat@luffy.cx>
2014-01-19 16:28:39 +0100

    prog: enable electric-indent-mode for programming modes

--- init.el
+++ init.el
@@ -76,6 +76,7 @@ be searched in \"el-get.el\" in the user Emacs directory."
 (defun vbe:customize-programming-language-mode ()
   (whitespace-mode 1)
   (hs-minor-mode 1)
+  (electric-indent-mode 1)
   (highlight-parentheses-mode 1))
 (add-hook 'prog-mode-hook ; This is the mode perl, makefile,
                           ; lisp-mode, scheme-mode, emacs-lisp-mode,
