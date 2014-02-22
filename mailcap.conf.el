846e0915e1df15748ee8754dd1043c7ff2b552a7
Vincent Bernat <bernat@luffy.cx>
2014-02-22 11:27:28 +0100

    mailcap: always rely on xdg-open but keep mime-types intact
    Instead of providing a minimal mailcap, we keep it as is but replace
    each program with xdg-open.

--- mailcap.conf.el
+++ mailcap.conf.el
@@ -1,9 +1,16 @@
-;; Use xdg-open, always.
-(setq mailcap-mime-data '(("application" (".*"
-                                          (viewer . "xdg-open '%s'")))
-                          ("image" (".*"
-                                   (viewer . "xdg-open '%s'")))
-                          ("text" ("html"
-                                   (viewer . "xdg-open '%s'"))))
-      mailcap-parsed-p t)
+(require 'dash)
 
+;; Use xdg-open, always.
+(mailcap-parse-mailcaps)
+(setq mailcap-mime-data
+      (--map (cons (car it)
+                   (--map (cons (car it)
+                                (--map (let ((key (car it))
+                                             (value (cdr it)))
+                                         (if (and (eq key 'viewer)
+                                                  (stringp value))
+                                             '(viewer . "xdg-open '%s'")
+                                           it))
+                                       (cdr it)))
+                          (cdr it)))
+             mailcap-mime-data))
