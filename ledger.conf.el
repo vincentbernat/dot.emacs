bcd4ab72d2a35bd0f6655b802a911f79fc3c0674
Vincent Bernat <bernat@luffy.cx>
2014-03-23 01:25:33 +0100

    ledger: make it usable with a dark background

---
+++ ledger.conf.el
@@ -0,0 +1,6 @@
+;; Fonts for ledger are for light background, modify them
+
+(set-face-attribute 'ledger-font-xact-highlight-face nil
+                    :background "grey30")
+(set-face-attribute 'ledger-occur-xact-face nil
+                    :background "grey30")
