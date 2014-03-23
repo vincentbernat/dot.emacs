ccc3455ffe7a5f49eb6b342f367e7a920e4bbadc
Vincent Bernat <bernat@luffy.cx>
2014-03-23 01:40:50 +0100

    ledger: more custom fonts for dark background

--- ledger.conf.el
+++ ledger.conf.el
@@ -1,6 +1,10 @@
 ;; Fonts for ledger are for light background, modify them
 
 (set-face-attribute 'ledger-font-xact-highlight-face nil
-                    :background "grey30")
+                    :background "grey10")
 (set-face-attribute 'ledger-occur-xact-face nil
-                    :background "grey30")
+                    :background "grey10")
+(set-face-attribute 'ledger-font-payee-cleared-face nil
+                    :foreground "grey70")
+(set-face-attribute 'ledger-font-other-face nil
+                    :foreground "violet")
