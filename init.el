36077111360723937331c286fe9b1ec42a5982b7
Vincent Bernat <vbe@deezer.com>
2014-04-11 18:02:49 +0200

    prog: disable-electric-indent-mode for rst-mode as well

--- init.el
+++ init.el
@@ -89,7 +89,7 @@ be searched in \"el-get.el\" in the user Emacs directory."
 ;; Disable electric indent mode on some mode
 (defun vbe:no-electric-indent-mode ()
   (set (make-local-variable 'electric-indent-mode) nil))
-(let ((modes '(python-mode markdown-mode)))
+(let ((modes '(python-mode markdown-mode rst-mode)))
   (dolist (mode modes)
     (add-hook
       (intern (concat (symbol-name mode) "-hook"))
