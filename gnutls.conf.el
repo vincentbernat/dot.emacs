cc8509ebf4b127e4c868b5b45fd5a4807f039046
Vincent Bernat <vincent@bernat.im>
2014-05-18 12:51:22 +0200

    gnutls: don't use too low DH parameter
    I don't understand why the default value for this is 256...

--- gnutls.conf.el
+++ gnutls.conf.el
@@ -3,3 +3,5 @@
   (gnutls-negotiate :process (open-network-stream name buffer host service)
                     :hostname host
                     :verify-hostname-error t :verify-error t))
+
+(setq gnutls-min-prime-bits 1024)
