65c277b1a46ce1bb4476e8fcce2d73568f87f1fb
Vincent Bernat <vincent@bernat.im>
2014-06-08 17:47:32 +0200

    files: put auto-save files in a dedicated directory
    It seems that the prefix cannot just be a directory. Add something for
    that.

--- files.conf.el
+++ files.conf.el
@@ -14,7 +14,7 @@
 (global-auto-revert-mode 1)   ; Auto revert (when no pending changes)
 
 ;; Backups and auto saves
-(let ((tmp (vbe:run-directory "saves")))
+(let ((tmp (concat (vbe:run-directory "saves") "/saves-")))
   (setq make-backup-files nil     ; Don't make backups, not used in ages
         auto-save-list-file-prefix tmp  ; Autosave in a dedicated directory
         auto-save-file-name-transforms
