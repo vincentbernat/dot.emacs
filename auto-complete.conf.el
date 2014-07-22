111a1cce2268babd70f230890390af429b6e734b
Vincent Bernat <vincent@bernat.im>
2014-07-22 18:18:42 +0200

    auto-complete: try a workaround for flycheck
    This doesn't work. :(

--- auto-complete.conf.el
+++ auto-complete.conf.el
@@ -10,3 +10,19 @@
 ;; Don't use up/down arrow (use M-n, M-p only) to browse list
 (define-key ac-completing-map [down] nil)
 (define-key ac-completing-map [up] nil)
+
+(defvar vbe:ac-flycheck-poll-completion-end-timer nil
+  "Timer to poll end of completion.")
+(defun vbe:ac-syntax-checker-workaround ()
+  (make-variable-buffer-local 'vbe:ac-flycheck-poll-completion-end-timer)
+  (when (require 'flycheck nil t)
+    (defadvice flycheck-handle-idle-change (around vbe:ac-flycheck-stop-advice activate)
+      (if ac-completing
+          (setq vbe:ac-flycheck-poll-completion-end-timer
+                (run-at-time 0.5
+                             nil
+                             #'flycheck-handle-idle-change))
+        ad-do-it)))
+    (when (featurep 'flycheck)
+      (ad-disable-advice 'flycheck-handle-idle-change 'around 'vbe:ac-flycheck-stop-advice)))
+(vbe:ac-syntax-checker-workaround)
