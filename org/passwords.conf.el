e22989c6a222fe8c4ae641f3fd4b7f5ebe2de57c
Vincent Bernat <vincent@bernat.im>
2014-08-24 13:48:54 +0200

    org-passwords: add some bindings to org-passwords-mode

--- org/passwords.conf.el
+++ org/passwords.conf.el
@@ -5,4 +5,11 @@
       org-passwords-random-words-dictionary "/usr/share/dict/american-english"
       org-passwords-time-opened "5 min")
 
+(define-key org-passwords-mode-map
+  (kbd "C-c u")
+  'org-passwords-copy-username)
+(define-key org-passwords-mode-map
+  (kbd "C-c p")
+  'org-passwords-copy-password)
+
 ;;; passwords.conf.el ends here
