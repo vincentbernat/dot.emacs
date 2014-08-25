00cb7e19b20abd3ef41ef3788bbdebf4466e95cb
Vincent Bernat <vincent@bernat.im>
2014-08-25 14:39:59 +0200

    org-passwords: add a binding to browse URL associated with an entry

--- org/passwords.conf.el
+++ org/passwords.conf.el
@@ -5,11 +5,27 @@
       org-passwords-random-words-dictionary "/usr/share/dict/american-english"
       org-passwords-time-opened "5 min")
 
+(defun vbe:org-passwords-web-browse ()
+  "Browse the URL of the current entry."
+  (interactive)
+  (save-restriction
+    (save-excursion
+      (org-narrow-to-subtree)
+      (search-backward-regexp "^\\*")
+      (search-forward-regexp (concat "^[[:space:]]*:URL:[[:space:]]*"))
+      (browse-url (buffer-substring-no-properties (point)
+                                                  (funcall (lambda ()
+                                                             (end-of-line)
+                                                             (point))))))))
+
 (define-key org-passwords-mode-map
   (kbd "C-c u")
   'org-passwords-copy-username)
 (define-key org-passwords-mode-map
   (kbd "C-c p")
   'org-passwords-copy-password)
+(define-key org-passwords-mode-map
+  (kbd "C-c g")
+  'vbe:org-passwords-web-browse)
 
 ;;; passwords.conf.el ends here
