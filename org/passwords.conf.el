49b5b21b440b926485e411c255c92827c0f07911
Vincent Bernat <vincent@bernat.im>
2014-08-25 20:54:33 +0200

    org-passwords: merge upstream changes

--- org/passwords.conf.el
+++ org/passwords.conf.el
@@ -5,19 +5,6 @@
       org-passwords-random-words-dictionary "/usr/share/dict/american-english"
       org-passwords-time-opened "5 min")
 
-(defun vbe:org-passwords-web-browse ()
-  "Browse the URL of the current entry."
-  (interactive)
-  (save-restriction
-    (save-excursion
-      (org-narrow-to-subtree)
-      (search-backward-regexp "^\\*")
-      (search-forward-regexp (concat "^[[:space:]]*:URL:[[:space:]]*"))
-      (browse-url (buffer-substring-no-properties (point)
-                                                  (funcall (lambda ()
-                                                             (end-of-line)
-                                                             (point))))))))
-
 (define-key org-passwords-mode-map
   (kbd "C-c u")
   'org-passwords-copy-username)
@@ -26,6 +13,6 @@
   'org-passwords-copy-password)
 (define-key org-passwords-mode-map
   (kbd "C-c g")
-  'vbe:org-passwords-web-browse)
+  'org-passwords-open-url)
 
 ;;; passwords.conf.el ends here
