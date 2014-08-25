6e83d565934f7971b827e73911a988e79bbbc3a1
Vincent Bernat <vincent@bernat.im>
2014-08-26 01:05:22 +0200

    org-passwords: reduce time spent in password buffer

--- org/passwords.conf.el
+++ org/passwords.conf.el
@@ -4,7 +4,7 @@
 (require 'org-passwords)
 (setq org-passwords-file "~/Documents/org/passwords.gpg"
       org-passwords-random-words-dictionary "/usr/share/dict/american-english"
-      org-passwords-time-opened "5 min")
+      org-passwords-time-opened "2 min")
 
 (define-key org-passwords-mode-map
   (kbd "C-c d u")
