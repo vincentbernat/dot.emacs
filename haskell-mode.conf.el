a3c7936814135845648ac5ce4c5701f64a6b9f32
Vincent Bernat <vincent@bernat.im>
2014-09-27 00:22:44 +0200

    haskell: configure interaction and indentation

---
+++ haskell-mode.conf.el
@@ -0,0 +1,9 @@
+;;; Code:
+;; haskell-mode configuration
+
+(add-hook 'haskell-mode-hook
+          'haskell-indentation-mode)
+(add-hook 'haskell-mode-hook
+          'interactive-haskell-mode)
+
+;;; haskell-mode.conf.el ends here
