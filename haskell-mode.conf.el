9a3828d01fdadb615d7eff7043a8ee805137c4f2
Vincent Bernat <vincent@bernat.im>
2014-10-02 17:54:00 +0200

    haskell: use cabal to execute repl

--- haskell-mode.conf.el
+++ haskell-mode.conf.el
@@ -5,5 +5,6 @@
           'haskell-indentation-mode)
 (add-hook 'haskell-mode-hook
           'interactive-haskell-mode)
+(setq haskell-program-name "cabal repl")
 
 ;;; haskell-mode.conf.el ends here
