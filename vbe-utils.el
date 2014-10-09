35d97d3c4c932a44abfb78399c57869840fae9d9
Vincent Bernat <vincent@bernat.im>
2014-10-09 17:04:12 +0200

    utils: use a minor mode for edit-region stuff

--- vbe-utils.el
+++ vbe-utils.el
@@ -69,6 +69,12 @@ substituting hyphens for slashes."
       (kill-region beg end)
       (insert resulting-text))))
 
+(defvar vbe:edit-region-mode-map (make-sparse-keymap))
+(define-key vbe:edit-region-mode-map "\C-c'" 'vbe:edit-region-exit)
+(define-key vbe:edit-region-mode-map "\C-xk" 'vbe:edit-region-exit)
+(define-minor-mode vbe:edit-region-mode
+  "Minor mode to edit region with another mode")
+
 ;; Edit region in another buffer.  This could have been done with just
 ;; an indirect buffer and narrowing but we want to remove and restore
 ;; any indentation. For example, this allows one to edit a block
@@ -81,6 +87,7 @@ substituting hyphens for slashes."
                                         (apropos-internal "-mode$" 'commandp))
                                 nil t))
                  nil))
+         (wincfg (current-window-configuration))
          (code (buffer-substring-no-properties start end))
          (left-margin (vbe:compute-left-margin code))
          (original (current-buffer))
@@ -108,12 +115,13 @@ substituting hyphens for slashes."
       (setq-local vbe:edit-region--ovl ovl)
       (setq-local vbe:edit-region--original original)
       (setq-local vbe:edit-region--left-margin left-margin)
+      (setq-local vbe:edit-region--wincfg wincfg)
 
       ;; Remove file name to not allow saving
       (setq buffer-file-name nil)
 
-      ;; How to exit
-      (local-set-key "\C-c'" 'vbe:edit-region-exit))
+      ;; Set minor mode
+      (vbe:edit-region-mode))
     (pop-to-buffer buffer)
     (message (substitute-command-keys
               "Edit, then exit with \\[vbe:edit-region-exit]"))))
@@ -135,6 +143,7 @@ substituting hyphens for slashes."
         (ovl vbe:edit-region--ovl)
         (original vbe:edit-region--original)
         (left-margin vbe:edit-region--left-margin)
+        (wincfg vbe:edit-region--wincfg)
         code)
     (indent-rigidly (point-min) (point-max) left-margin)
     (setq code (buffer-substring-no-properties (point-min) (point-max)))
@@ -146,6 +155,8 @@ substituting hyphens for slashes."
         (insert code)))
     (set-buffer-modified-p nil)
     (kill-buffer (current-buffer))
-    (switch-to-buffer original)))
+    (switch-to-buffer original)
+    (when wincfg
+      (set-window-configuration org-edit-src-saved-temp-window-config))))
 
 (provide 'vbe/utils)
