fb392a6782460ddc2e6ede3aabe74e31882c56e9
Vincent Bernat <vincent@bernat.im>
2014-10-23 15:23:57 +0200

    clojure: enable cider-mode as well

--- clojure-mode.conf.el
+++ clojure-mode.conf.el
@@ -1,3 +1,10 @@
+;;; Code:
+
 ;; For some reason, `clojure-test-maybe-enable' is autoloaded and add
 ;; a silly hook to clojure-mode triggering an error. Remove this hook.
 (remove-hook 'clojure-mode-hook 'clojure-test-maybe-enable)
+
+;; Use cider-mode as well
+(add-hook 'clojure-mode-hook 'cider-mode)
+
+;;; clojure-mode.conf.el ends here
