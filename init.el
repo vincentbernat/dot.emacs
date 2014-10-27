08d134f1b3b86edcacb63a1ecb306cc59d8fd185
Vincent Bernat <vincent@bernat.im>
2014-10-27 16:17:48 +0100

    init: do not disable electric-indent-mode for python
    The support is quite better in Emacs 24.4

--- init.el
+++ init.el
@@ -91,8 +91,7 @@ be searched in \"el-get.el\" in the user Emacs directory."
 ;; Disable electric indent mode on some mode
 (defun vbe:no-electric-indent-mode ()
   (set (make-local-variable 'electric-indent-mode) nil))
-(let ((modes '(python-mode
-               markdown-mode rst-mode yaml-mode
+(let ((modes '(markdown-mode rst-mode yaml-mode
                ledger-mode message-mode
                fundamental-mode)))
   (dolist (mode modes)
