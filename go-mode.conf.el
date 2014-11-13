ff8579f6fcb1ce048500054d86c111068e55442a
Vincent Bernat <vincent@bernat.im>
2014-11-13 20:57:36 +0100

    go: try harder to set a sensible GOPATH

--- go-mode.conf.el
+++ go-mode.conf.el
@@ -8,14 +8,19 @@
          (root (if (projectile-project-p)
                    (projectile-project-root)
                  default-directory))
+         (altroot (locate-dominating-file default-directory "Makefile"))
          (additionals (list root
-                            (format "%sbuild" root)))
-         (new-gopath (append (--filter (file-accessible-directory-p it) additionals)
+                            (format "%sbuild" root)
+                            (and altroot (expand-file-name altroot))
+                            (and altroot (format "%sbuild" (expand-file-name altroot)))))
+         (new-gopath (append (-filter 'file-accessible-directory-p (-non-nil additionals))
                              initial-gopath))
          (gopath (mapconcat 'identity new-gopath ":")))
     (make-local-variable 'process-environment)
     (setq process-environment (cons (format "GOPATH=%s" gopath) process-environment))))
 
 (add-hook 'go-mode-hook 'vbe:go-mode-setup-gopath)
+
+(locate-dominating-file "/home/bernat/code/deezer/dashkiosk/" "Makefile")
 
 ;;; go-mode.conf.el ends here
