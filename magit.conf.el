0a17bd83a84b35a0d32dcbf3ea2d59c12c3ef528
Vincent Bernat <vincent@bernat.im>
2015-07-08 21:54:14 +0200

    magit: remove fullscreen stuff
    This doesn't work anymore...

--- magit.conf.el
+++ magit.conf.el
@@ -1,18 +1,3 @@
-(setq magit-diff-refine-hunk nil)
-
-;; full screen magit-status
-;; Stolen from: http://whattheemacsd.com/setup-magit.el-01.html
-(defadvice magit-status (around vbe:magit-fullscreen activate)
-  (window-configuration-to-register :vbe/magit-fullscreen)
-  ad-do-it
-  (delete-other-windows))
-(defun vbe:magit-quit-session ()
-  "Restores the previous window configuration and kills the magit buffer"
-  (interactive)
-  (kill-buffer)
-  (jump-to-register :vbe/magit-fullscreen))
-(define-key magit-status-mode-map (kbd "q") 'vbe:magit-quit-session)
-
 (defun vbe:magit-insert-recent-commits ()
   (magit-git-insert-section (recent "Recent commits:")
       (apply-partially 'magit-wash-log 'unique)
