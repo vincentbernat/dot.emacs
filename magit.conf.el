528f3d53f57d3868f13ac5b01d099d8de4cdafab
Vincent Bernat <vincent@bernat.im>
2015-07-08 21:52:36 +0200

    magit: remove inexistent variables
    Those variables don't exist anymore with magit 2.x

--- magit.conf.el
+++ magit.conf.el
@@ -1,8 +1,4 @@
-;; Where is located the code?
-(setq magit-repo-dirs (list (expand-file-name "~/code"))
-      magit-repo-dirs-depth 2
-      magit-stage-all-confirm nil       ; don't confirm stage all
-      magit-diff-refine-hunk nil)
+(setq magit-diff-refine-hunk nil)
 
 ;; full screen magit-status
 ;; Stolen from: http://whattheemacsd.com/setup-magit.el-01.html
