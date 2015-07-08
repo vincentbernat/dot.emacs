cdfbff57c0c28b3c3052cbd2f73fb0fcd085bc78
Vincent Bernat <vincent@bernat.im>
2015-07-08 21:57:32 +0200

    magit: use builtin magit-insert-recent-commits function
    Don't need a custom one anymore!

--- magit.conf.el
+++ magit.conf.el
@@ -1,9 +1,4 @@
-(defun vbe:magit-insert-recent-commits ()
-  (magit-git-insert-section (recent "Recent commits:")
-      (apply-partially 'magit-wash-log 'unique)
-    "log" "--format=format:%h %s" "-n" "10"))
-
 ;; Add a "latest commits" section
 (magit-add-section-hook 'magit-status-sections-hook
-                        'vbe:magit-insert-recent-commits
+                        'magit-insert-recent-commits
                         'magit-insert-unpushed-commits t)
