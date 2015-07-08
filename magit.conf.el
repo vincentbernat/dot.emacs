fc1ac0431de882f0b312b3fe36814cff926b55bd
Vincent Bernat <vincent@bernat.im>
2015-07-08 22:00:58 +0200

    magit: don't check commit styles
    I am very good at writing commits. Thank you.

--- magit.conf.el
+++ magit.conf.el
@@ -2,3 +2,6 @@
 (magit-add-section-hook 'magit-status-sections-hook
                         'magit-insert-recent-commits
                         'magit-insert-unpushed-commits t)
+
+(remove-hook 'git-commit-finish-query-functions
+             'git-commit-check-style-conventions)
