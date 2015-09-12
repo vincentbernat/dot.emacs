a57672ed8b3b9ad57f251af0aa7aa06f046f6094
Vincent Bernat <vincent@bernat.im>
2015-09-13 00:24:50 +0200

    paredit: enable for emacs-lisp-mode
    And emacs-lisp-mode is part of lisp-mode.

--- emacs-lisp-mode.conf.el
+++
@@ -1,5 +0,0 @@
-;;; Code:
-
-(add-hook 'emacs-lisp-mode-hook #'paredit-mode)
-
-;;; emacs-lisp-mode.conf.el ends here
--- lisp-mode.conf.el
+++ lisp-mode.conf.el
@@ -1,5 +1,6 @@
 ;;; Code:
 
 (add-hook 'lisp-mode-hook #'paredit-mode)
+(add-hook 'emacs-lisp-mode-hook #'paredit-mode)
 
 ;;; lisp-mode.conf.el ends here
