20e536bf080f51929a5c265e9ca603bb62bc76d9
Vincent Bernat <vincent@bernat.im>
2015-09-16 10:04:49 +0200

    dtrt-indent: don't mangle the modeline

---
+++ dtrt-indent.conf.el
@@ -0,0 +1,6 @@
+;;; Code:
+
+;; Remove the dtrt indent stuff from modeline, no need for it.
+(setq global-mode-string (remove 'dtrt-indent-mode-line-info global-mode-string))
+
+;;; dtrt-indent.conf.el ends here
