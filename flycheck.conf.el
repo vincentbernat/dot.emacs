78463ed7f1de4a5f833118cd53eeb22f980152a2
Vincent Bernat <vincent@bernat.im>
2015-09-16 08:24:54 +0200

    flycheck: disable flycheck in ERC

--- flycheck.conf.el
+++ flycheck.conf.el
@@ -1,2 +1,5 @@
-;; Use a dot file to avoid being detected by some watchers
-(setq flycheck-temp-prefix ".flycheck")
+(setq
+ ;; Use a dot file to avoid being detected by some watchers
+ flycheck-temp-prefix ".flycheck"
+ ;; Don't enable flycheck on some modes
+ flycheck-global-modes '(not erc-mode))
