a4ae05446ab1c43c5e45fb26cd6c56313c2b60b8
Vincent Bernat <vincent@bernat.im>
2015-09-26 21:36:38 +0200

    company: don't steal up/down arrows

--- company.conf.el
+++ company.conf.el
@@ -2,6 +2,10 @@
 
 (diminish 'company-mode)
 
+;; Don't use up/down arrow (use M-n, M-p only) to browse list
+(define-key company-active-map [down] nil)
+(define-key company-active-map [up] nil)
+
 ;; Additional backends
 (add-to-list 'company-backends 'company-anaconda)
 
