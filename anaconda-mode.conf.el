4773017b54d64bbeae96b591beedd1328d1bd2d0
Vincent Bernat <vincent@bernat.im>
2015-09-29 20:31:09 +0200

    python: move anaconda runtime into run/

---
+++ anaconda-mode.conf.el
@@ -0,0 +1,7 @@
+;;; Code:
+
+(setq anaconda-mode-installation-directory
+      (expand-file-name "run/anaconda-mode"
+                        user-emacs-directory))
+
+;;; anaconda-mode.conf.el ends here
