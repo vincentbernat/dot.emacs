3f5d8b8522b16897af8ccd9328c50f9ce5b7c287
Vincent Bernat <vincent@bernat.im>
2015-10-28 16:25:19 +0100

    beacon: don't blink in ERC and in Gnus

--- beacon.conf.el
+++ beacon.conf.el
@@ -5,4 +5,9 @@
       beacon-blink-duration 0.3
       beacon-color "#f57900")
 
+;;; Disable on some modes
+(add-to-list 'beacon-dont-blink-major-modes 'erc-mode)
+(add-to-list 'beacon-dont-blink-major-modes 'gnus-group-mode)
+(add-to-list 'beacon-dont-blink-major-modes 'gnus-summary-mode)
+
 ;;; beacon.conf.el ends here
