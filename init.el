5e0d3f376fd8b79a7c0d8949a3d29b3a04a45a6d
Vincent Bernat <vincent@bernat.im>
2015-11-01 12:55:53 +0100

    js: use js2-jsx-mode for JSX files

--- init.el
+++ init.el
@@ -81,7 +81,7 @@ be searched in \"el-get.el\" in the user Emacs directory."
 (add-to-list 'auto-mode-alist '("/Rakefile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.html?$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
 (add-to-list 'auto-mode-alist '("\\.scala\\'" . scala-mode))
 (add-to-list 'magic-mode-alist '("#!/usr/bin/env node" . js2-mode))
