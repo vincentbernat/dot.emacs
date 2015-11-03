384fd5d42907e5953c9e4fe7a5557666a9ab6127
Vincent Bernat <vincent@bernat.im>
2015-11-03 22:29:34 +0100

    cider: use clj-refactor

--- cider-mode.conf.el
+++ cider-mode.conf.el
@@ -6,4 +6,7 @@
 ;; Enable paredit on REPL
 (add-hook 'cider-repl-mode-hook #'paredit-mode)
 
+;; Keybindings for clj-refactor
+(cljr-add-keybindings-with-prefix "\C-cr")
+
 ;;; cider-mode.conf.el ends here
