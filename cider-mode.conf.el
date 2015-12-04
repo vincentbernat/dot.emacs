16571b19964467825f306a40c88517344f5760ed
Vincent Bernat <vincent@bernat.im>
2015-12-04 18:18:28 +0100

    cider: change spinner type to be fixed width

--- cider-mode.conf.el
+++ cider-mode.conf.el
@@ -6,4 +6,7 @@
 ;; Enable paredit on REPL
 (add-hook 'cider-repl-mode-hook #'paredit-mode)
 
+;; Change the spinner type to stay at constant-width
+(setq cider-eval-spinner-type 'vertical-breathing)
+
 ;;; cider-mode.conf.el ends here
