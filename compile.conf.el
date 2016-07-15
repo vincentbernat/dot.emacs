56075a4529dcedf7250e3d26ad62977fb0895e58
Vincent Bernat <vincent@bernat.im>
2016-07-15 17:01:46 +0200

    compile: fix ANSI color in compile buffer

--- compile.conf.el
+++ compile.conf.el
@@ -4,6 +4,6 @@
 (defun vbe:colorize-compilation-buffer ()
   (when (eq major-mode 'compilation-mode)
     (ansi-color-apply-on-region compilation-filter-start (point-max))))
-(add-hook 'compilation-filter-hook 'my-colorize-compilation-buffer)
+(add-hook 'compilation-filter-hook 'vbe:colorize-compilation-buffer)
 
 ;;;
