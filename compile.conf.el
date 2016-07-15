2925964679b33fd33b6f3a260df55338c876d1e3
Vincent Bernat <vincent@bernat.im>
2016-07-15 16:57:08 +0200

    compile: display ANSI colors when compiling

---
+++ compile.conf.el
@@ -0,0 +1,9 @@
+;;; Code:
+
+(require 'ansi-color)
+(defun vbe:colorize-compilation-buffer ()
+  (when (eq major-mode 'compilation-mode)
+    (ansi-color-apply-on-region compilation-filter-start (point-max))))
+(add-hook 'compilation-filter-hook 'my-colorize-compilation-buffer)
+
+;;;
