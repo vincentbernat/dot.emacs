9a65738a6c50474df3016794cbb8b10382a85d37
Vincent Bernat <vincent@bernat.im>
2016-07-17 07:38:35 +0200

    emojify: remove variable emojify-image-dir
    This is now derived automatically from emojify-emojis-dir.

--- emojify.conf.el
+++ emojify.conf.el
@@ -1,3 +1,2 @@
 (setq emojify-emojis-dir (expand-file-name "run/emojis"
-                                           user-emacs-directory)
-      emojify-image-dir (expand-file-name emojify-emoji-set emojify-emojis-dir))
+                                           user-emacs-directory))
