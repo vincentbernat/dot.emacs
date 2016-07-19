8fb73f9fb4a97c652be927bdb8523a20ada05fbd
Vincent Bernat <vincent@bernat.im>
2016-07-19 15:53:06 +0200

    go: rework the way we handle GOPATH

--- go-mode.conf.el
+++ go-mode.conf.el
@@ -3,24 +3,23 @@
 (require 'dash)
 
 (defun vbe:go-mode-setup-gopath ()
-  "Setup GOPATH to the root of the project + build."
-  (let* ((initial-gopath (split-string (getenv "GOPATH") ":"))
-         (root (if (projectile-project-p)
-                   (projectile-project-root)
-                 default-directory))
-         (altroot (locate-dominating-file default-directory "Makefile"))
-         (additionals (list root
-                            (format "%sbuild" root)
-                            (format "%sbuild~" root)
-                            (and altroot (expand-file-name altroot))
-                            (and altroot (format "%sbuild" (expand-file-name altroot)))
-                            (and altroot (format "%sbuild~" (expand-file-name altroot)))))
-         (new-gopath (append (-filter 'file-accessible-directory-p (-non-nil additionals))
-                             initial-gopath))
-         (gopath (mapconcat 'identity new-gopath ":")))
+  "Setup GOPATH from guessed possibilities."
+  (let ((gopath (or (go-guess-gopath (current-buffer))
+                    (go-original-gopath))))
     (make-local-variable 'process-environment)
     (setq process-environment (cons (format "GOPATH=%s" gopath) process-environment))))
 
+
+(add-to-list 'go-guess-gopath-functions #'vbe:go-build-gopath)
+
+(defun vbe:go-build-gopath ()
+  (let* ((d (locate-dominating-file buffer-file-name ".gopath"))
+         (src (concat d (file-name-as-directory ".gopath"))))
+    (if (and d
+             (file-exists-p src))
+        (list src))))
+
+(locate-dominating-file "/home/bernat/code/exoscale/jura/logger/root.go" ".gopath")
 (add-hook 'go-mode-hook 'vbe:go-mode-setup-gopath)
 (add-hook 'before-save-hook 'gofmt-before-save)
 
