d02be56bb4267f0bdbe126f3070e82c756f32839
Vincent Bernat <vincent@bernat.im>
2016-07-19 15:57:27 +0200

    go: remove debug code

--- go-mode.conf.el
+++ go-mode.conf.el
@@ -9,17 +9,14 @@
     (make-local-variable 'process-environment)
     (setq process-environment (cons (format "GOPATH=%s" gopath) process-environment))))
 
-
-(add-to-list 'go-guess-gopath-functions #'vbe:go-build-gopath)
-
 (defun vbe:go-build-gopath ()
   (let* ((d (locate-dominating-file buffer-file-name ".gopath"))
          (src (concat d (file-name-as-directory ".gopath"))))
     (if (and d
              (file-exists-p src))
         (list src))))
+(add-to-list 'go-guess-gopath-functions #'vbe:go-build-gopath)
 
-(locate-dominating-file "/home/bernat/code/exoscale/jura/logger/root.go" ".gopath")
 (add-hook 'go-mode-hook 'vbe:go-mode-setup-gopath)
 (add-hook 'before-save-hook 'gofmt-before-save)
 
