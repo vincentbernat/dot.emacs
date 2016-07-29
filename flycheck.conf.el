36f172acf369583cc590c85632db5e2152037bde
Vincent Bernat <vincent@bernat.im>
2016-07-29 17:58:30 +0200

    flycheck: speedup go build by using -i

--- flycheck.conf.el
+++ flycheck.conf.el
@@ -1,5 +1,14 @@
+;;; Code:
+
 (setq
  ;; Use a dot file to avoid being detected by some watchers
  flycheck-temp-prefix ".flycheck"
  ;; Don't enable flycheck on some modes
  flycheck-global-modes '(not erc-mode))
+
+;; Go
+(setq
+ ;; Speedup compilation by saving intermediate files.
+ flycheck-go-build-install-deps t)
+
+;;;
