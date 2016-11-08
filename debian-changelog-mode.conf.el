02dae3542a3b6222956214e3435281d09bda9066
Vincent Bernat <vincent@bernat.im>
2016-11-08 13:53:31 +0100

    debian:patch debian-changelog-date-string function
    Don't use current locale.

--- debian-changelog-mode.conf.el
+++ debian-changelog-mode.conf.el
@@ -4,3 +4,9 @@
 ;; Add UNRELEASE at the front place
 (add-to-list 'debian-changelog-allowed-distributions "UNRELEASED" nil
              (lambda (a b) nil))
+
+;; See https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=803767
+(advice-add 'debian-changelog-date-string :around
+            (lambda (f)
+              (let ((system-time-locale "C"))
+                (funcall f))))
