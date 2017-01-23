9353bf062cbd99360697d37fa21004cc86035950
Vincent Bernat <vincent@bernat.im>
2017-01-23 08:17:08 +0100

    go-mode: factor a bit GOPATH guess function

--- go-mode.conf.el
+++ go-mode.conf.el
@@ -1,6 +1,7 @@
 ;;; Code:
 
 (require 'dash)
+(require 'cl)
 
 (defun vbe:go-mode-setup-gopath ()
   "Setup GOPATH from guessed possibilities."
@@ -11,17 +12,17 @@
 
 ;; I am allergic to the GOPATH concept. I use kludge to work around them.
 (defun vbe:custom-gopath ()
-  (or (vbe:custom-gopath- ".gopath") (vbe:custom-gopath- ".gopath~")))
-(defun vbe:custom-gopath- (gopath)
   "Guess gopath if we have a GOPATH along with a `vendor'
 directory. In this case, GOPATH is both of them. This usually
 requires to have a symlink in vendor with `src' pointing back to
 `vendor'."
-  (let* ((d (locate-dominating-file buffer-file-name gopath))
-         (src (concat d (file-name-as-directory "vendor"))))
-    (if (and d
-             (file-exists-p src))
-        (list (concat d (file-name-as-directory gopath)) src))))
+  (cl-letf ((add (gopath)
+                 (let* ((d (locate-dominating-file buffer-file-name gopath))
+                        (src (concat d (file-name-as-directory "vendor"))))
+                   (if (and d
+                            (file-exists-p src))
+                       (list (concat d (file-name-as-directory gopath)) src)))))
+    (or (add ".gopath") (add ".gopath~"))))
 (add-to-list 'go-guess-gopath-functions 'vbe:custom-gopath)
 
 (add-hook 'go-mode-hook 'vbe:go-mode-setup-gopath)
