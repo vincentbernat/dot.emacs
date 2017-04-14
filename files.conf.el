63c3912914f89c52c3836ad4687c96b4d022c567
Vincent Bernat <vincent@bernat.im>
2017-04-14 08:16:50 +0200

    files: remove code only needed with Emacs < 24.4

--- files.conf.el
+++ files.conf.el
@@ -1,21 +1,5 @@
 ;;; Code:
 
-;; Try to emulate auto revert with undo history
-(when (version< emacs-version "24.4")
-  (defun vbe:revert-buffer-keep-history (&rest -)
-    "Revert buffer but keep undo history"
-    (clear-visited-file-modtime)
-    (widen)
-    (let ((inhibit-read-only t)
-          (current-point (point)))
-      (erase-buffer)
-      (insert-file-contents (buffer-file-name))
-      (not-modified)
-      (goto-char current-point)
-      (set-visited-file-modtime)))
-
-  (setq revert-buffer-function 'vbe:revert-buffer-keep-history))
-
 (global-auto-revert-mode 1)   ; Auto revert (when no pending changes)
 
 ;; Backups and auto saves
