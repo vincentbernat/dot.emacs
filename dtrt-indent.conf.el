baa2d538b65c1d5a3dc022c971145330005f27d1
Vincent Bernat <vincent@bernat.im>
2018-06-28 19:12:06 +0200

    dtrt-indent: adjust offset for groovy mode

--- dtrt-indent.conf.el
+++ dtrt-indent.conf.el
@@ -3,4 +3,7 @@
 ;; Remove the dtrt indent stuff from modeline, no need for it.
 (setq global-mode-string (remove 'dtrt-indent-mode-line-info global-mode-string))
 
+;; Add more offset variables
+(add-to-list 'dtrt-indent-hook-mapping-list '(groovy-mode c/c++/java groovy-indent-offset))
+
 ;;; dtrt-indent.conf.el ends here
