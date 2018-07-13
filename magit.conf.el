9a15301c0cc9595d2cf36dba81208cbba3273e67
Vincent Bernat <vincent@bernat.im>
2018-07-13 10:18:13 +0200

    magit: always show recent commits, but only once
    New versions of magit were either displaying unpushed commits or
    recent ones.

--- magit.conf.el
+++ magit.conf.el
@@ -1,7 +1,11 @@
 ;; Add a "latest commits" section
 (magit-add-section-hook 'magit-status-sections-hook
                         'magit-insert-recent-commits
-                        'magit-insert-unpushed-commits t)
+                        nil t)
+(magit-add-section-hook 'magit-status-sections-hook
+                        'magit-insert-unpushed-to-upstream
+                        'magit-insert-unpushed-to-upstream-or-recent t)
+(remove-hook 'magit-status-sections-hook 'magit-insert-unpushed-to-upstream-or-recent)
 
 (remove-hook 'git-commit-finish-query-functions
              'git-commit-check-style-conventions)
