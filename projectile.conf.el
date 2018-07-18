cb540df6dc4b045e1a7a0ab2affdf8ba899ba5c2
Vincent Bernat <vincent@bernat.im>
2018-07-18 23:18:17 +0200

    projectile: remove SVN specific stuff
    Projectile knows how to handle that now.

--- projectile.conf.el
+++ projectile.conf.el
@@ -4,10 +4,6 @@
   (setq projectile-known-projects-file (expand-file-name "bookmarks.eld" dir)
         projectile-cache-file (expand-file-name "projectile.cache" dir)))
 
-;; Detect SVN projects
-(add-to-list 'projectile-project-root-files ".svn")
-(add-to-list 'projectile-globally-ignored-directories ".svn")
-
 (setq projectile-completion-system 'ivy)
 
 (diminish 'projectile-mode)
