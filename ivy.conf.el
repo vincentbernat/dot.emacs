4b575c211114ad86e42e82a36ff350227d0051b6
Vincent Bernat <vincent@bernat.im>
2018-07-20 10:24:42 +0200

    ivy: use an arrow to highlight position of current item
    When there is only 2-3 items and the second one is selected, it is not
    immediately obvious which one is highlighted.

--- ivy.conf.el
+++ ivy.conf.el
@@ -1,4 +1,16 @@
 (diminish 'ivy-mode)
 
+(defun vbe:ivy-format-function-arrow (cands)
+  "Transform CANDS into a string for minibuffer with an unicode arrow prefix."
+  (ivy--format-function-generic
+   (lambda (str)
+     (concat "▶ " (ivy--add-face str 'ivy-current-match)))
+   (lambda (str)
+     (concat "  " str))
+   cands
+   "\n"))
+
 (setq ivy-use-virtual-buffers nil
-      ivy-count-format "(%d/%d) ")
+      ivy-count-format "(%d/%d) "
+      ivy-format-function #'vbe:ivy-format-function-arrow)
+
