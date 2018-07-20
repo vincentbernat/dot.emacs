d202eddafca44e001ea4ec6cf3302e34f96a1b71
Vincent Bernat <vincent@bernat.im>
2018-07-20 20:38:18 +0200

    ivy: Alias C-RET to M-RET in minibuffer

--- ivy.conf.el
+++ ivy.conf.el
@@ -15,5 +15,7 @@
       ivy-extra-directories nil
       ivy-format-function #'vbe:ivy-format-function-arrow)
 
-;; Alias for S-SPC
+;; Alias for S-SPC (restrict to current matches)
 (define-key ivy-minibuffer-map (kbd "C-SPC") 'ivy-restrict-to-matches)
+;; Alias for M-RET (call action without exiting)
+(define-key ivy-minibuffer-map (kbd "<C-return>") 'ivy-call)
