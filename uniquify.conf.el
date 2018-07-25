7116ff6e789a45accfc64b6eae9680e77fbf6c20
Vincent Bernat <vincent@bernat.im>
2018-07-25 07:03:33 +0200

    uniquify: switch to forward mode

--- uniquify.conf.el
+++ uniquify.conf.el
@@ -1,4 +1,4 @@
-(setq uniquify-buffer-name-style 'reverse
+(setq uniquify-buffer-name-style 'forward
       uniquify-separator "/"
       uniquify-after-kill-buffer-p t ; rename after killing
       uniquify-ignore-buffers-re "^\\*")
