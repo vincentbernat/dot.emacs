a2a7b8d535c352fd76f42f1803cd3a529d527f92
Vincent Bernat <vincent@bernat.ch>
2018-09-10 07:48:35 +0200

    Revert "packages: install paradox package manager"
    This reverts commit ef463cbb08f74eb217f7c0fb3a9844d7ea33813a.

--- lisp/vbe-ergonomics.el
+++ lisp/vbe-ergonomics.el
@@ -163,10 +163,6 @@
 (use-package ibuffer
   :bind (("C-x C-b" . ibuffer)))
 
-;; paradox: better list-package
-(use-package paradox
-  :commands (paradox-list-packages))
-
 ;; Vertical alignment
 (use-package align
   :bind (("C-c |" . align-current)))
