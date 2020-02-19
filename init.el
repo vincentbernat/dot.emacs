b01941f31c2d4b77de1dd11043d2b8451dbacf9f
Vincent Bernat <vincent@bernat.ch>
2020-02-19 08:10:11 +0100

    init: tune GC for faster startup

--- init.el
+++ init.el
@@ -21,6 +21,14 @@
 
 ;(package-initialize)
 
+;; Tune GC for faster startup
+(setq gc-cons-threshold most-positive-fixnum ; 2^61 bytes
+      gc-cons-percentage 0.6)
+(add-hook 'emacs-startup-hook
+  (lambda ()
+    (setq gc-cons-threshold 16777216 ; 16mb
+          gc-cons-percentage 0.1)))
+
 ;; Setup environment to load other files.
 
 (setq load-prefer-newer t)
