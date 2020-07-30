bd30d4c5a00a2ec997e185eb45a7162340630d2a
Vincent Bernat <vincent@bernat.ch>
2020-07-30 07:59:57 +0200

    ergonomics: ensure transient pkg does not litter .emacs with files

--- lisp/vbe-ergonomics.el
+++ lisp/vbe-ergonomics.el
@@ -225,6 +225,12 @@ point reaches the beginning or end of the buffer, stop there."
 (put 'upcase-region 'disabled nil)
 (put 'narrow-to-page 'disabled nil)
 
+;; Miscellaneous tweaks for packages
+(use-package transient
+  :ensure nil :defer t :custom
+  transient-levels-file (vbe:runtime-file "transient" "levels.el")
+  transient-values-file (vbe:runtime-file "transient" "values.el")
+  transient-history-file (vbe:runtime-file "transient" "history.el"))
 
 ;; Miscellaneous tweaks for non-packages
 (use-package tramp
