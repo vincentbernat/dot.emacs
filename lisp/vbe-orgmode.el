e47e08c90decce09deede7163257273dfb3f40ca
Vincent Bernat <vincent@bernat.ch>
2022-09-13 10:36:56 +0200

    orgmode: try org-modern

--- lisp/vbe-orgmode.el
+++ lisp/vbe-orgmode.el
@@ -25,14 +25,12 @@
 (require 'org-tempo)
 
 (setq
- org-startup-indented t                 ; indent by default
- org-tags-column -75                    ; align tags on the 70th columns
+ org-startup-indented nil               ; don't indent by default
  org-hide-leading-stars t               ; don't show leading stars
  org-cycle-separator-lines 0            ; don't show blank lines between collapsed trees
  org-src-fontify-natively t             ; fontify code blocks
  org-edit-src-content-indentation 0     ; don't indent source blocks
  org-catch-invisible-edits 'error       ; don't edit invisible text
- org-pretty-entities t                  ; use "pretty entities"
  org-export-use-babel nil               ; do not evaluate blocks when exporting
  org-image-actual-width (when window-system (list (truncate (* (frame-native-width) 0.9))))
  org-export-with-toc nil                ; do not add a ToC when exporting
@@ -103,10 +101,16 @@
                        vbe:git-auto-commit-mode-for-orgmode-directories))
       (git-auto-commit-mode 1))))
 
-(use-package org-bullets
-  :hook (org-mode . org-bullets-mode)
+(use-package org-modern
+  :hook (org-mode . org-modern-mode)
   :config
-  (setq org-bullets-bullet-list '("✺" "✹" "✸" "✷" "✶" "✭" "✦" "■" "▲" "●" )))
+  (setq org-auto-align-tags nil
+        org-tags-column 0
+        org-special-ctrl-a/e t
+        org-insert-heading-respect-content t
+        org-hide-emphasis-markers t
+        org-pretty-entities t
+        org-ellipsis "…"))
 
 (use-package org-mime
   :defer t)
