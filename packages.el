74ade2e557b24a61021547fcf91d6a68f1971b41
Vincent Bernat <vincent@bernat.ch>
2022-11-02 14:23:31 +0100

    packages: move to misc stuff not related to a major mode

--- packages.el
+++ packages.el
@@ -7,14 +7,14 @@
 ;; Org-related packages
 (package! org-download)
 
-;; Modes
+;; Major modes
 (package! debian-el)
 (package! dockerfile-mode)
 (package! dpkg-dev-el)
-(package! git-auto-commit-mode)
-(package! openbsd-knf-style :recipe (:host github :repo "hogand/openbsd-knf-emacs"))
 (package! protobuf-mode)
 (package! puppet-mode)
 
-;; Mis packages
+;; Misc packages
 (package! bm)
+(package! git-auto-commit-mode)
+(package! openbsd-knf-style :recipe (:host github :repo "hogand/openbsd-knf-emacs"))
