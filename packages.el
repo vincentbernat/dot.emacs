3dc09ec14ffc768e4e3013629101262787451d8e
Vincent Bernat <vincent@bernat.ch>
2022-11-03 14:08:46 +0100

    packages: stop pinning doom-modeline

--- packages.el
+++ packages.el
@@ -1,9 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-;; Doom-related packages
-(package! doom-modeline :pin "b66d5e5006df4cedb1119da3d83fd6c08965b83")
-
 ;; Org-related packages
 (package! org-download)
 
