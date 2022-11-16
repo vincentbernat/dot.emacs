ad31d39d5fcc8ba3b877aa3b2cccc873e8f7fd18
Vincent Bernat <vincent@bernat.ch>
2022-11-16 22:07:36 +0100

    packages: pin all packages

--- packages.el
+++ packages.el
@@ -1,26 +1,32 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
+;; Packages can be updated by using M-x doom/bump-package-at-point.
+
 ;; Org-related packages
-(package! org-download)
+(package! org-download :pin "19e166f0a8c539b4144cfbc614309d47a9b2a9b7")
 
 ;; Major modes
-(package! debian-el)
-(package! dpkg-dev-el)
-(package! dockerfile-mode)
-(package! lua-mode)
-(package! protobuf-mode)
-(package! puppet-mode)
-(package! junos-mode :recipe (:host github
-                              :repo "vincentbernat/junos-mode"
-                              :files (:defaults "junos.py")))
+(package! debian-el :pin "a3ef20c269b9192710567571b20718f572942bc4")
+(package! dpkg-dev-el :pin "458f5230d02b15c94e94eca1af4eabaec30f45db")
+(package! dockerfile-mode :pin "52c6c00da1d31c0b6c29c74335b3af63ed6bf06c")
+(package! lua-mode :pin "3e783c93aa8a3d3ca985686438aa8d140cbddae6")
+(package! protobuf-mode :pin "c862c1cab4d591963437774cc7d3f0a8e4bb1da1")
+(package! puppet-mode :pin "71bcd383f20a457e8ad34e0e08ec47f8e1b64263")
+(package! junos-mode
+  :recipe (:host github
+           :repo "vincentbernat/junos-mode"
+           :files (:defaults "junos.py"))
+  :pin "e2e7f7224fff89f38f2b02f5431299d95654b380")
 (package! tide :disable t)
 (package! anaconda-mode :disable t)
 
 ;; Misc packages
-(package! apheleia)
-(package! bm)
-(package! git-auto-commit-mode)
-(package! openbsd-knf-style :recipe (:host github
-                                     :repo "vincentbernat/openbsd-knf-emacs"))
+(package! apheleia :pin "33d4542b58476d50f01464576664de1acea1f62f")
+(package! bm :pin "9a31c61f44e6f1033ca43bd7f3eb33ffdb2ca595")
+(package! git-auto-commit-mode :pin "a6b6e0fa183be381463e2b44ef128db1b6c4234b")
+(package! openbsd-knf-style
+  :recipe (:host github
+           :repo "vincentbernat/openbsd-knf-emacs")
+  :pin "1144dd016dd0e0fd096073db2492bb5814e8ada8")
 (package! lsp-mode :pin "f5d521d56cfef54d0f102680e956a856347d2c96")
