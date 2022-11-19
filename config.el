2a5f0e05f8b313236f63f5795fe48a6528f08267
Vincent Bernat <vincent@bernat.ch>
2022-11-20 00:06:30 +0100

    config(edit-indirect): remove it
    There seems to be some related stuff in Doom. Maybe I don't need that!

--- config.el
+++ config.el
@@ -103,26 +103,6 @@
    projectile-files-cache-expire 10)
   (pushnew! projectile-project-root-files "go.mod" "flakes.nix"))
 
-;; Edit indirect allows to edit a region into a separate buffer
-(map! :leader :desc "Edit indirect region" "'" #'edit-indirect-region)
-(after! edit-indirect
-  (defvar vbe:edit-indirect--left-margin 0)
-  (defun vbe:compute-left-margin (code)
-    "Compute left margin of a string of CODE."
-    (-min
-     (-map #'(lambda (line) (length (car (s-match "^\\s-*" line))))
-           (-remove 's-blank? (s-lines code)))))
-  (defun vbe:after-indirect-edit-remove-left-margin ()
-    "Remove left-margin and save it into a local variable."
-    (let ((lm (vbe:compute-left-margin (buffer-substring (point-min) (point-max)))))
-      (indent-rigidly (point-min) (point-max) (* -1 lm))
-      (setq-local vbe:edit-indirect--left-margin lm)))
-  (defun vbe:after-indirect-edit-restore-left-margin ()
-    "Restore left-margin before commiting."
-    (indent-rigidly (point-min) (point-max) vbe:edit-indirect--left-margin))
-  (add-hook 'edit-indirect-after-creation-hook #'vbe:after-indirect-edit-remove-left-margin)
-  (add-hook 'edit-indirect-before-commit-hook #'vbe:after-indirect-edit-restore-left-margin))
-
 ;; Bookmarks
 (map! :leader
       (:prefix ("b" . "bookmarks")
