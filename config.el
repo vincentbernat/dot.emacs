8fd466413d4cf1f777215e3c77e9e28609021da1
Vincent Bernat <vincent@bernat.ch>
2022-11-20 00:50:15 +0100

    config(company): make tab accept current completion

--- config.el
+++ config.el
@@ -74,7 +74,9 @@
 (after! company
   ;; Trigger company-mode explicitely
   (setq company-idle-delay nil)
-  (map! :map company-mode-map [remap indent-for-tab-command] #'company-indent-or-complete-common))
+  (map!
+   :map company-mode-map [remap indent-for-tab-command] #'company-indent-or-complete-common
+   :map company-active-map "<tab>" #'company-complete-selection))
 
 ;; Format
 (after! apheleia
