31add7095ef79aca534a8f07890b05949ace17ce
Vincent Bernat <vincent@bernat.ch>
2024-02-14 08:12:49 +0100

    packages: update some packages

--- packages.el
+++ packages.el
@@ -4,11 +4,11 @@
 ;; Packages can be updated by using M-x doom/bump-package-at-point.
 
 ;; Major modes
-(package! debian-el :pin "c7cb03299bcbdefc591f3f624f91479c4797da19")
-(package! dpkg-dev-el :pin "af9aad721cb263e495e2f77df458e9496549c04b")
+(package! debian-el :pin "fcae715f72fc76ad9f196efbe1010faab49c7781")
+(package! dpkg-dev-el :pin "827a2e6b78ccc17668a822966aacbcd5ceba3abc")
 (package! dockerfile-mode :pin "52c6c00da1d31c0b6c29c74335b3af63ed6bf06c")
 (package! lua-mode :pin "d074e4134b1beae9ed4c9b512af741ca0d852ba3")
-(package! protobuf-mode :pin "c05b320d9c18173bfce36c4bef22f9953d340ff9")
+(package! protobuf-mode :pin "a02a8b88c16c4889eae0d2c951afab72caf14968")
 (package! puppet-mode :pin "71bcd383f20a457e8ad34e0e08ec47f8e1b64263")
 (package! junos-mode
   :recipe (:host github
@@ -17,18 +17,18 @@
   :pin "e2e7f7224fff89f38f2b02f5431299d95654b380")
 (package! systemd :pin "8742607120fbc440821acbc351fda1e8e68a8806")
 (package! jinja2-mode :pin "03e5430a7efe1d163a16beaf3c82c5fd2c2caee1")
-(package! adoc-mode :pin "05914c7319ac5c8a7e68047d5bb982d691a0d535")
+(package! adoc-mode :pin "524e3aec6c4afbb41cff7515119a3f2a3e3dc942")
 (package! yang-mode :pin "4b4ab4d4a79d37d6c31c6ea7cccbc425e0b1eded")
 
 ;; Misc packages
-(package! apheleia :pin "8f512dba3ee2eabae0dbfd8289b89d54c132cb87")
+(package! apheleia :pin "c07e90793c839d1973a0820d24e03a0a6b30b77e")
 (package! bm :pin "1351e2e15a7666e614c94b41414c8f024dc10a50")
 (package! git-auto-commit-mode :pin "a7b59acea622a737d23c783ce7d212fefb29f7e6")
 (package! openbsd-knf-style
   :recipe (:host github
            :repo "vincentbernat/openbsd-knf-emacs")
   :pin "1144dd016dd0e0fd096073db2492bb5814e8ada8")
-(package! pass :pin "b1198478cd62e1f21d5072d0275e2c446b88b15b")
+(package! pass :pin "ed7031c5c33a384d07da2d15c9d5f854027a26a2")
 (package! org-download :pin "19e166f0a8c539b4144cfbc614309d47a9b2a9b7")
 
 ;; Disable some packages I don't want
